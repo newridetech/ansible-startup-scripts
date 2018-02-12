93b9e1d2224b696facca741cf65389a75a1a42e2
Piotr Piskiewicz <piotr.piskiewicz@newride.tech>
2018-02-12 01:09:21 +0100

    git repo link

--- setup.sh
+++ setup.sh
@@ -10,6 +10,6 @@ export DB_ROOT_PASSWORD=""
 apt-get install python-pip
 pip install ansible
 mkdir /tmp/ansible_startup
-git clone git@github.com:newridetech/ansible-startup-scripts.git /tmp/ansible_startup
+git clone git://github.com/newridetech/ansible-startup-scripts.git /tmp/ansible_startup
 ansible-playbook -i 'localhost,' --connection=local /tmp/ansible_startup/presetup.yml
 ansible-playbook -i 'localhost,' --connection=local /tmp/ansible_startup/setup.yml
